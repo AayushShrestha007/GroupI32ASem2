dd1ce1e613ca3bc77401dc3661274f4e5781fd18
preeya9 <aryalpriya9@gmail.com>
2022-11-20 16:43:38 +0545

    database updated

--- logindb.sql
+++ logindb.sql
@@ -2,13 +2,15 @@ create database logindb;
 use logindb;
 
 create table user(
+user_id int auto_increment unique,
 username varchar(30),
-password varchar(20)
+email varchar(30),
+password varchar(20),
+primary key(user_id)
 );
 select * from user;
 drop table user;
 
 insert into user (username,password) values 
-("preeya","123456"),
-("aryalpreeya9@gmail.com","3456")
+("preeya","aryalprapti@gmail.com","123456")
 ;
