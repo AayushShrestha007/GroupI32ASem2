dd80c2f6d0c5e7ebef872b63a0a41ba3d188458e
Smritikhadgi <smritikhadgi98@gmail.com>
2022-12-29 21:32:08 +0545

    refresh

--- src/view/Reviewbookingscreen.java
+++ src/view/Reviewbookingscreen.java
@@ -345,6 +345,7 @@ public class Reviewbookingscreen extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        //Refresh
         setVisible(false);
         setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
