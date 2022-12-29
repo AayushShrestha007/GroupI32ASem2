332c144aa4c6773ce3a8227a919843fc943e1fbf
Smritikhadgi <smritikhadgi98@gmail.com>
2022-12-29 20:34:39 +0545

    Refresh

--- src/view/confirmationPage.java
+++ src/view/confirmationPage.java
@@ -194,6 +194,7 @@ public class confirmationPage extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        //Refresh
         setVisible(false);
         setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
