5fc7288790a17ab1937b4da6324960ff818ac765
Pramesh167 <prameshpathak2002@gmail.com>
2022-12-29 18:10:10 +0545

    18915

--- src/view/privacypolicy.java
+++ src/view/privacypolicy.java
@@ -117,6 +117,7 @@ public class privacypolicy extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        //close after pressing cancle button
         
         dispose();
         
